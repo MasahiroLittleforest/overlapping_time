1e42cf48569878e968215399987c3009e45575d6
MasahiroLittleforest <masahiro.littleforest@gmail.com>
2020-11-05 23:09:05 +0900

    solved import error

--- lib/src/models/time_of_day_range_pair.dart
+++ lib/src/models/time_of_day_range_pair.dart
@@ -1,5 +1,5 @@
 import 'package:overlapping_time/overlapping_time.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
 
 import '../extensions/time_of_day_extension.dart';
 
